26d3759bc70d414ff94edcfb408a5f93883ed2f8
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-06-18 13:15:56 +0900

    포인트 사용 기능 서비스 테스트

--- src/test/kotlin/io/hhplus/tdd/application/PointServiceTest.kt
+++ src/test/kotlin/io/hhplus/tdd/application/PointServiceTest.kt
@@ -85,53 +85,53 @@ class PointServiceTest {
         }
     }
 
-    @Nested
-    @DisplayName("[사용] 회원별 포인트 사용 서비스")
-    inner class UseUserPointTests {
-
+    @Nested 
+    @DisplayName("[사용] 회원별 포인트 사용 서비스") 
+    inner class UseUserPointTests { 
+ 
         @Test
-        fun `특정 유저의 아이디와 사용할 포인트를 받으면 domain 레이어에서 사용하고 남은 값을 받아온다`() {
-            // given
-            val userId = 1L
-            val amount = 100L
-            val initialUserPoint = UserPoint(id = userId, point = 1000L, updateMillis = System.currentTimeMillis())
-            val remainPoint = initialUserPoint.point - amount
-            val remainUserPoint = UserPoint(id = userId, point = remainPoint, updateMillis = System.currentTimeMillis())
+        fun `특정 유저의 아이디와 사용할 포인트를 받으면 domain 레이어에서 사용하고 남은 값을 받아온다`() { 
+            // given 
+            val userId = 1L 
+            val amount = 100L 
+            val initialUserPoint = UserPoint(id = userId, point = 1000L, updateMillis = System.currentTimeMillis()) 
+            val remainPoint = initialUserPoint.point - amount 
+            val remainUserPoint = UserPoint(id = userId, point = remainPoint, updateMillis = System.currentTimeMillis()) 
+ 
+            // when 
+            `when`(pointService.getUserPointById(userId)).thenReturn(initialUserPoint) 
+            `when`(point.useUserPoint(userId, amount, remainPoint)).thenReturn(remainUserPoint) 
+            val result = pointService.useUserPoint(userId, amount) 
+ 
+            // then 
+            assertThat(result).isEqualTo(remainUserPoint) 
+        } 
 
-            // when
-            `when`(pointService.getUserPointById(userId)).thenReturn(initialUserPoint)
-            `when`(point.useUserPoint(userId, amount, remainPoint)).thenReturn(remainUserPoint)
-            val result = pointService.useUserPoint(userId, amount)
-
-            // then
-            assertThat(result).isEqualTo(remainUserPoint)
-        }
-
-        @Test
-        fun `사용하려는 포인트가 0 보다 작은수가 전달되면 예외(IllegalArgumentException)를 발생 시킨다`() {
-            val userId = 1L
-            val amount = -100L
-
-            assertThatThrownBy {
-                pointService.useUserPoint(userId, amount)
-            }.isInstanceOf(IllegalArgumentException::class.java)
-                .hasMessage("포인트는 0 이하일 수 없습니다")
-        }
-
-        @Test
-        fun `포인트 잔액이 부족할때 포인트 사용 요청이 들어오면 예외(NotEnoughPointException)를 발생 시킨다`() {
-            val userId = 1L
-            val amount = 100L
-            val initialUserPoint = UserPoint(id = userId, point = 0, updateMillis = System.currentTimeMillis())
-
-            `when`(pointService.getUserPointById(userId)).thenReturn(initialUserPoint)
-
-            assertThatThrownBy {
-                pointService.useUserPoint(userId, amount)
-            }.isInstanceOf(NotEnoughPointException::class.java)
-                .hasMessage("사용 가능한 포인트가 부족합니다")
-        }
-    }
+        @Test 
+        fun `사용하려는 포인트가 0 보다 작은수가 전달되면 예외(IllegalArgumentException)를 발생 시킨다`() { 
+            val userId = 1L 
+            val amount = -100L 
+ 
+            assertThatThrownBy { 
+                pointService.useUserPoint(userId, amount) 
+            }.isInstanceOf(IllegalArgumentException::class.java) 
+                .hasMessage("포인트는 0 이하일 수 없습니다") 
+        } 
+ 
+        @Test 
+        fun `포인트 잔액이 부족할때 포인트 사용 요청이 들어오면 예외(NotEnoughPointException)를 발생 시킨다`() { 
+            val userId = 1L 
+            val amount = 100L 
+            val initialUserPoint = UserPoint(id = userId, point = 0, updateMillis = System.currentTimeMillis()) 
+ 
+            `when`(pointService.getUserPointById(userId)).thenReturn(initialUserPoint) 
+ 
+            assertThatThrownBy { 
+                pointService.useUserPoint(userId, amount) 
+            }.isInstanceOf(NotEnoughPointException::class.java) 
+                .hasMessage("사용 가능한 포인트가 부족합니다") 
+        } 
+    } 
 
     @Nested
     @DisplayName("[조회] 회원별 포인트 사용내역 조회 서비스")
