e29b2e339fda5b54d56cad2665dd6165ef810ac5
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-06-18 13:17:35 +0900

    포인트 사용 기능 infra layer repository

--- src/main/kotlin/io/hhplus/tdd/infra/PointRepository.kt
+++ src/main/kotlin/io/hhplus/tdd/infra/PointRepository.kt
@@ -6,6 +6,6 @@ import io.hhplus.tdd.point.UserPoint
 interface PointRepository {
     fun findById(userId: Long): UserPoint
     fun chargeUserPoint(userId: Long, amount: Long, totalPointsAfterCharge: Long): UserPoint
-    fun useUserPoint(userId: Long, amount: Long, remainingPoints : Long): UserPoint
+    fun useUserPoint(userId: Long, amount: Long, remainingPoints : Long): UserPoint 
     fun getUserPointHistories(userId: Long): List<PointHistory>
 }
